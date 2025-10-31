5461737fc749b0b09ae5189ccc2cf36cc4dffc93
Ryan Nguyen <nguyen@kemilinks.net>
2025-10-31 06:37:27 +0000

    change api gateway name

--- apigateway.tf
+++ apigateway.tf
@@ -1,5 +1,5 @@
 resource "aws_api_gateway_rest_api" "api" {
   # checkov:skip=CKV_AWS_237:Ensure Create before destroy for API Gateway - Not Compliant
-  name        = "${var.name_prefix}-vessel-tracking-api-${local.workspace_safe}"
+  name        = "${var.name_prefix}-api-${local.workspace_safe}"
   description = "API for Capstone Project"
 }
