b02562ca73bf7c0d25f0d5b814b910622472b742
Ryan Nguyen <nguyen@kemilinks.net>
2025-11-03 08:42:00 +0000

    fiz lambda permission

--- topupcard_lambda.tf
+++ topupcard_lambda.tf
@@ -42,5 +42,5 @@ resource "aws_lambda_permission" "apigw_invoke" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.topupcard_lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/POST/cards/*/topup"
 }
