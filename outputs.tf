845a04e8a771e0f72a72aa0c309a44e41f957936
Ryan Nguyen <nguyen@kemilinks.net>
2025-11-03 08:23:55 +0000

    change output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "api_invoke_url" {
-  value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${provider.aws.region}.amazonaws.com/${local.workspace_safe}"
+  value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.region}.amazonaws.com/${local.workspace_safe}"
 }
