26550196f944209d2103ae79cf7868b0c3d42480
Ryan Nguyen <nguyen@kemilinks.net>
2025-11-04 06:38:40 +0000

    fix S3 bucket name

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "static_bucket" {
-  bucket        = "${var.name_prefix}-${local.s3_bucket_name}"
+  bucket        = "${var.name_prefix}-${local.bucket_name}"
   force_destroy = true
   tags = {
-    Name = "${var.name_prefix}-${local.s3_bucket_name}"
+    Name = "${var.name_prefix}-${local.bucket_name}"
   }
 }
