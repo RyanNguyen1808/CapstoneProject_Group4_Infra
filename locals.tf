e345c71ee74992ef0b778cd16c112f7dce263b9e
Ryan Nguyen <nguyen@kemilinks.net>
2025-12-13 10:32:36 +0000

    fix vpc

--- locals.tf
+++ locals.tf
@@ -3,5 +3,5 @@ locals {
   workspace_safe        = lower(terraform.workspace)
   alternate_domain_name = "${var.name_prefix}.${local.workspace_safe}.${var.domain}"
   www_domain_name       = "www.${var.name_prefix}.${local.workspace_safe}.${var.domain}"
-  s3_origin_id          = "${aws_s3_bucket.static_bucket.bucket}-Origin"
+  //s3_origin_id          = "${aws_s3_bucket.static_bucket.bucket}-Origin"
 }
