2ea647c0b9f991620f6923a4814ba712e6c8fdc2
Ryan Nguyen <nguyen@kemilinks.net>
2025-12-13 23:21:02 +0000

    add lambda

---
+++ vpc-endpoints.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc_endpoint" "logs" {
+  vpc_id             = module.vpc.vpc_id
+  service_name       = "com.amazonaws.${data.aws_region.current.name}.logs"
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = module.vpc.private_subnets
+  security_group_ids = [aws_security_group.lambda_sg.id]
+}
+
+resource "aws_vpc_endpoint" "secretsmanager" {
+  vpc_id             = module.vpc.vpc_id
+  service_name       = "com.amazonaws.${data.aws_region.current.name}.secretsmanager"
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = module.vpc.private_subnets
+  security_group_ids = [aws_security_group.lambda_sg.id]
+}
