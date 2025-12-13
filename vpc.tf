3e3aaf4c42876f75d9b9aa48987648aca54d6794
Ryan Nguyen <nguyen@kemilinks.net>
2025-12-13 12:30:29 +0000

    fix vpc

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.5" # or latest stable
 
-  enable_flow_logs = false
+  enable_flow_log = false
 
   name = "${var.name_prefix}-${local.workspace_safe}-vpc"
   cidr = "10.0.0.0/16"
