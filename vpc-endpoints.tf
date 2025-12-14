a64da4c98ba22e5a140eae0680e65bfc20261f66
Ryan Nguyen <nguyen@kemilinks.net>
2025-12-14 02:05:41 +0000

    change enpoinds private dns

--- vpc-endpoints.tf
+++ vpc-endpoints.tf
@@ -3,17 +3,19 @@ data "aws_region" "current" {
 }
 
 resource "aws_vpc_endpoint" "logs" {
-  vpc_id             = module.vpc.vpc_id
-  service_name       = "com.amazonaws.${data.aws_region.current.name}.logs"
-  vpc_endpoint_type  = "Interface"
-  subnet_ids         = module.vpc.private_subnets
-  security_group_ids = [aws_security_group.lambda_sg.id]
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.logs"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = module.vpc.private_subnets
+  security_group_ids  = [aws_security_group.lambda_sg.id]
+  private_dns_enabled = true
 }
 
 resource "aws_vpc_endpoint" "secretsmanager" {
-  vpc_id             = module.vpc.vpc_id
-  service_name       = "com.amazonaws.${data.aws_region.current.name}.secretsmanager"
-  vpc_endpoint_type  = "Interface"
-  subnet_ids         = module.vpc.private_subnets
-  security_group_ids = [aws_security_group.lambda_sg.id]
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.secretsmanager"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = module.vpc.private_subnets
+  security_group_ids  = [aws_security_group.lambda_sg.id]
+  private_dns_enabled = true
 }
