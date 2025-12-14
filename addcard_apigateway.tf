2a743751bdb9948a75ed949b2d0b7dd4a1f1bd08
Ryan Nguyen <nguyen@kemilinks.net>
2025-12-14 06:08:03 +0000

    fix add card api

--- addcard_apigateway.tf
+++ addcard_apigateway.tf
@@ -1,3 +1,7 @@
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
 # API Resource for /card
 resource "aws_api_gateway_resource" "card" {
   rest_api_id = aws_api_gateway_rest_api.api.id
