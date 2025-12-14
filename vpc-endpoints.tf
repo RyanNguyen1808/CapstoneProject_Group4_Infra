a2d5928b0dc21dd5ab3dfdbe243edeccb08776f6
Ryan Nguyen <nguyen@kemilinks.net>
2025-12-14 03:01:15 +0000

    add security group for endpoint sm

--- vpc-endpoints.tf
+++ vpc-endpoints.tf
@@ -16,6 +16,23 @@ resource "aws_vpc_endpoint" "secretsmanager" {
   service_name        = "com.amazonaws.${data.aws_region.current.name}.secretsmanager"
   vpc_endpoint_type   = "Interface"
   subnet_ids          = module.vpc.private_subnets
-  security_group_ids  = [aws_security_group.lambda_sg.id]
+  security_group_ids  = [aws_security_group.secrets_endpoint_sg.id]
   private_dns_enabled = true
+}
+
+# Security group for the Secrets Manager endpoint
+resource "aws_security_group" "secrets_endpoint_sg" {
+  name        = "${var.name_prefix}-secrets-endpoint-sg-${local.workspace_safe}"
+  description = "SG for Secrets Manager VPC endpoint"
+  vpc_id      = module.vpc.vpc_id
+}
+
+# Ingress rule to allow Lambda SG to connect
+resource "aws_security_group_rule" "lambda_to_sm_endpoint" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.secrets_endpoint_sg.id
+  source_security_group_id = aws_security_group.lambda_sg.id
 }
