82d5f74e846c25ddf49bb9184c714732a2c2659f
Ryan Nguyen <nguyen@kemilinks.net>
2025-12-16 04:24:03 +0000

    fix checkov

--- addcard_apigateway.tf
+++ addcard_apigateway.tf
@@ -103,6 +103,7 @@ resource "aws_api_gateway_integration_response" "add_card_integration_response" 
 
 # CORS START: OPTIONS method for preflight
 resource "aws_api_gateway_method" "add_options" {
+  # checkov:skip=CKV2_AWS_53: "Ensure AWS API gateway request is validated"
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.add.id
   http_method   = "OPTIONS"
