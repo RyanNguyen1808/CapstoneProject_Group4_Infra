141cf24c89822196f427a2cd61935ab28248bef8
Ryan Nguyen <nguyen@kemilinks.net>
2025-12-16 04:38:35 +0000

    enable cors for get transasction history api

--- transactionhistory_apigateway.tf
+++ transactionhistory_apigateway.tf
@@ -27,6 +27,85 @@ resource "aws_api_gateway_integration" "getTransactionHistory_integration" {
   uri                     = aws_lambda_function.getTransactionHistory_lambda.invoke_arn
 }
 
+resource "aws_api_gateway_method_response" "getTransactionHistory_post_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.getTransactionHistory.id
+  http_method = aws_api_gateway_method.getTransactionHistory_post.http_method
+  status_code = "200"
+
+  # CORS START
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+  }
+  # CORS END
+}
+
+resource "aws_api_gateway_integration_response" "getTransactionHistory_integration_response" {
+  rest_api_id       = aws_api_gateway_rest_api.api.id
+  resource_id       = aws_api_gateway_resource.getTransactionHistory.id
+  http_method       = aws_api_gateway_method.getTransactionHistory_post.http_method
+  status_code       = aws_api_gateway_method_response.getTransactionHistory_post_response_200.status_code
+  selection_pattern = ""
+
+  # CORS START
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'${var.allowed_origin}'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
+  }
+  # CORS END
+}
+
+resource "aws_api_gateway_method" "getTransactionHistory_options" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.getTransactionHistory.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "getTransactionHistory_options_mock" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.getTransactionHistory.id
+  http_method = aws_api_gateway_method.getTransactionHistory_options.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "getTransactionHistory_options_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.getTransactionHistory.id
+  http_method = aws_api_gateway_method.getTransactionHistory_options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "getTransactionHistory_options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.getTransactionHistory.id
+  http_method = aws_api_gateway_method.getTransactionHistory_options.http_method
+  status_code = aws_api_gateway_method_response.getTransactionHistory_options_response_200.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'${var.allowed_origin}'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
+  }
+
+  response_templates = {
+    "application/json" = ""
+  }
+}
+
 # Lambda permission
 resource "aws_lambda_permission" "apigw_getTransactionHistory" {
   statement_id  = "AllowAPIGatewayInvokeGetTransactionHistory"
