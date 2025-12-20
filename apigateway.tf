9be65c2a1a02012ce1b3f4187f54180416a677cf
Ryan Nguyen <nguyen@kemilinks.net>
2025-12-20 04:51:44 +0000

    change api to regional rather than edge opt

--- apigateway.tf
+++ apigateway.tf
@@ -2,6 +2,10 @@ resource "aws_api_gateway_rest_api" "api" {
   # checkov:skip=CKV_AWS_237:Ensure Create before destroy for API Gateway - Not Compliant
   name        = "${var.name_prefix}-api-${local.workspace_safe}"
   description = "API for Capstone Project"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
 }
 
 resource "aws_api_gateway_deployment" "api" {
